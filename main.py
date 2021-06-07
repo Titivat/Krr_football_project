fdea6d59e2461219cee09d18399c196a74cc4b93
titivat <titivatmo@gmail.com>
2021-06-07 08:58:14 +0700

    add: goal boundary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 import sys
+
+from pygame.constants import WINDOWHITTEST
 # --Init Pygame
 pygame.init()
 
@@ -22,16 +24,32 @@ BACKGROUND = pygame.transform.scale(BACKGROUND, (WINDOW_WIDTH, WINDOW_HEIGHT))
 game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('First Game')
 
+class Goal1(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 210))
+        self.image.fill(COLOR_BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.left = 23
+        self.rect.centery = WINDOW_HEIGHT / 2
+
+class OutLine1(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, WINDOW_HEIGHT))
+        self.image.fill(COLOR_WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.left = 22
+        self.rect.centery = WINDOW_HEIGHT / 2
+
 #--Sprite -- ##
-
-
 class Player1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((25, 60))
         self.image.fill(COLOR_RED)
         self.rect = self.image.get_rect()
-        self.rect.left = 25
+        self.rect.left = 35
         self.rect.centery = WINDOW_HEIGHT / 2
         self.dy = 0
 
@@ -56,7 +74,6 @@ class Player1(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom > WINDOW_HEIGHT:
             self.rect.bottom = WINDOW_HEIGHT
-
 
 class Player2(pygame.sprite.Sprite):
     def __init__(self):
@@ -89,16 +106,28 @@ class Player2(pygame.sprite.Sprite):
         if self.rect.bottom > WINDOW_HEIGHT:
             self.rect.bottom = WINDOW_HEIGHT
 
-
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 10))
+        self.image = pygame.Surface((15, 15))
         self.image.fill(COLOR_WHITE)
         self.rect = self.image.get_rect()
         self.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
         self.dx = random.choice([-1, 1])
         self.dy = random.choice([-2, -1, 1, 2])
+
+    def ballBounceY( self , player):
+        if(player == "player1"):
+            self.rect.x -= self.dx
+            self.dx *= -1
+            self.dx += random.choice([1, 2])
+            self.dx += 1
+
+        if(player == "player2"):
+            self.rect.x -= self.dx
+            self.dx *= -1
+            self.dx += random.choice([1, 2])
+            self.dx -= 1
 
     def update(self):
         self.rect.x += self.dx
@@ -106,6 +135,7 @@ class Ball(pygame.sprite.Sprite):
         # colusion
         if self.rect.top < 0:
             self.dy *= -1
+
         if self.rect.bottom > WINDOW_HEIGHT:
             self.dy *= -1
 
@@ -115,11 +145,26 @@ class Ball(pygame.sprite.Sprite):
             if(type(collion) == Player1):
                 self.rect.x -= self.dx
                 self.dx *= -1
-                self.dx += random.choice([0, 1])
+                self.dx += random.choice([1, 2])
+
+                if self.dx == 0:
+                    self.dx += 1
+
             if(type(collion) == Player2):
                 self.rect.x -= self.dx
                 self.dx *= -1
-                self.dx += random.choice([0, 1])
+                self.dx += random.choice([1, 2])
+
+                if self.dx == 0:
+                    self.dx -= 1
+
+            if(type(collion) == Goal1):
+                score.score1 += 1
+                ball.__init__()
+
+            if(type(collion) == OutLine1):
+                ball.ballBounceY('player1')
+
             # extra condition to make it fun
             if self.dy == 0:
                 self.dy += random.choice([-1, 1])
@@ -127,7 +172,6 @@ class Ball(pygame.sprite.Sprite):
                 self.dy += random.choice([-1, 0, 1])
             if self.dy >= 0:
                 self.dy += random.choice([-1, 0, 1])
-
 
 class Score():
     def __init__(self):
@@ -148,6 +192,7 @@ class Score():
         if ball.rect.left > WINDOW_WIDTH:
             self.score1 += 1
             ball.__init__()
+
         self.player1_score = self.score_font.render(
             str(self.score1), True, COLOR_WHITE, COLOR_BLACK)
         self.player2_score = self.score_font.render(
@@ -166,7 +211,6 @@ class Score():
             ball.dx = 0
             ball.dy = 0
 
-
 # --Sprite Groups
 all_sprites = pygame.sprite.Group()
 ball_sprite = pygame.sprite.GroupSingle()
@@ -175,43 +219,42 @@ ball_sprite = pygame.sprite.GroupSingle()
 player1 = Player1()
 all_sprites.add(player1)
 
-# player1
-position = 100
-for _ in range(3):
-    midPlayer1 = Player1()
-    midPlayer1.setPosition(250, position)
-    all_sprites.add(midPlayer1)
-    position += 300
-
-position = 100
-for _ in range(3):
-    midPlayer1 = Player1()
-    midPlayer1.setPosition(500, position)
-    all_sprites.add(midPlayer1)
-    position += 300
-
-# player2
-position = 100
-for _ in range(3):
-    midPlayer2 = Player2()
-    midPlayer2.setPosition(WINDOW_WIDTH - 250, position)
-    all_sprites.add(midPlayer2)
-    position += 300
-
-position = 100
-for _ in range(3):
-    midPlayer2 = Player2()
-    midPlayer2.setPosition(WINDOW_WIDTH - 500, position)
-    all_sprites.add(midPlayer2)
-    position += 300
-
-
 player2 = Player2()
 all_sprites.add(player2)
 
+position = 100
+midplayerPosition = 300
+attackerPosition = 600
+for _ in range(3):
+    midPlayer1 = Player1()
+    midPlayer1.setPosition(midplayerPosition, position)
+    all_sprites.add(midPlayer1)
+
+    midPlayer2 = Player2()
+    midPlayer2.setPosition(WINDOW_WIDTH - midplayerPosition, position)
+    all_sprites.add(midPlayer2)
+
+    attack1 = Player1()
+    attack1.setPosition(attackerPosition, position)
+    all_sprites.add(attack1)
+
+    attack2 = Player2()
+    attack2.setPosition(WINDOW_WIDTH - attackerPosition, position)
+    all_sprites.add(attack2)
+
+    position += 300
+
 # ball added
 ball = Ball()
-#ball_sprite.add( ball )
+ball_sprite.add(ball)
+
+#gal added
+outline = OutLine1()
+all_sprites.add(outline)
+
+goal1 = Goal1()
+all_sprites.add(goal1)
+
 
 # score
 score = Score()
@@ -228,6 +271,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            print( pos )
 
     # Update the movement of the sprite
     all_sprites.update()
